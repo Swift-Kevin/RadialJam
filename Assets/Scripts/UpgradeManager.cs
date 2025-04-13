90b0c24896fe5a36e1846290084ec4d2d9f0a73c
degamebeast <44504214+degamebeast@users.noreply.github.com>
2025-04-13 01:51:56 -0400

    basic upgrades added
    -delay decrease
    -score increase

--- Assets/Scripts/UpgradeManager.cs
+++ Assets/Scripts/UpgradeManager.cs
@@ -2,8 +2,87 @@ using UnityEngine;
 
 public class UpgradeManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+	public SaveData curData;
+
+	public long ClickProduce
+	{
+		get
+		{
+			byte tier = curData.upgradeData.productionTier;
+
+			switch(tier)
+			{
+				case < 5:
+					return tier;
+				case < 10:
+					return tier * 2;
+				case < 25:
+					return tier * (long)System.Math.Log(tier, 2);
+				case < 100:
+					return (long)System.Math.Pow(tier, 2);
+				default:
+					return (long)System.Math.Pow(100, 2);
+			}
+		}
+	}
+
+	public float ClickDelay
+	{
+		get
+		{
+			byte tier = curData.upgradeData.speedTier;
+
+			switch(tier)
+			{
+				case < 10:
+					return 1.0f/tier;
+				default:
+					return 0.1f;
+			}
+		}
+	}
+
+	public long AutoProduce
+	{
+		get
+		{
+			byte tier = curData.upgradeData.automationTier;
+
+			switch(tier)
+			{
+				case < 5:
+					return tier;
+				case < 10:
+					return tier * 2;
+				case < 25:
+					return (long)System.Math.Pow(tier,2);
+				case < 100:
+					return (long)System.Math.Pow(2, tier);
+				default:
+					return (long)System.Math.Pow(2, 100);
+			}
+		}
+	}
+
+	public float AutoDelay
+	{
+		get
+		{
+			byte tier = curData.upgradeData.autoSpeedTier;
+
+			switch(tier)
+			{
+				case < 10:
+					return 1.0f / tier;
+				default:
+					return 0.1f;
+			}
+		}
+	}
+
+	// Start is called once before the first execution of Update after the MonoBehaviour is created
+	void Start()
     {
         
     }
@@ -13,4 +92,13 @@ public class UpgradeManager : MonoBehaviour
     {
         
     }
+}
+
+[System.Serializable]
+public struct UpgradeData
+{
+    public byte productionTier;
+    public byte speedTier;
+    public byte automationTier;
+    public byte autoSpeedTier;
 }
