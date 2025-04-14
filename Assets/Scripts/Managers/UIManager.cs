3feedf6789d7cd977ca2e808dab224a4cd657f2c
Kevin Wall <kevinwall3186@gmail.com>
2025-04-14 18:46:20 -0400

    Dead

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -1,10 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.Audio;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public struct AudioData
