2a996e66714cd7a86c9218ce52aacc54ba0e92cb
degamebeast <44504214+degamebeast@users.noreply.github.com>
2025-04-14 13:12:25 -0400

    stuff

---
+++ Assets/Scripts/ClickerNumber.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+
+public class ClickerNumber
+{
+    public List<long> number;
+
+    private const long slotLimit = 1000000000000000000L;
+
+/*    public string Value
+    {
+        get
+        {
+
+        }
+    }*/
+
+}
